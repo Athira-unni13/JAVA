59ee54f1ad2a5558bdf5a5ee981aedf6632e0c36
Athira-unni13 <athiramvunnikrishnan1310@gmail.com>
2024-06-12 21:21:38 +0530

    my first commit

---
+++ first.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class first
+{
+	public static void main(String args[])
+	{
+		
+		String name;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("May i know your name");
+
+		name = sc.nextLine(); 		
+
+		System.out.println(" Welcome "+name);
+		
+	}	
+}
